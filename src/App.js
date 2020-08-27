d2968a7f7fa01cde66c5d22d1d32fa32efc865ee
holdenmad <holden.madagame@gmail.com>
2020-08-27 18:13:04 +0200

    Add basic non-working router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import React from 'react';
-import {Link, Route, Switch} from "react-router-dom"
-import './App.css';
-import Login from './components/Login'
-import Register from './components/Register'
-import Welcome from './components/Welcome'
-import Dashboard from './components/Dashboard'
-
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Welcome from "./components/Welcome";
+import Dashboard from "./components/Dashboard";
 
 function App() {
-
   return (
     <div className="App">
-     <Switch>
-                <Route exact path="/">
-                  <Welcome />
-                </Route>
-                <Route exact path="/register">
-                  <Register/>
-                </Route>
-                <Route exact path="/login">
-                  <Login />
-                </Route>
-                <Route exact path="/dashboard">
-                  <Dashboard />
-                </Route>
-
-          </Switch>
+      <Switch>
+        <Route exact path="/">
+          <Welcome />
+        </Route>
+        <Route exact path="/register">
+          <Register />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
     </div>
   );
 }

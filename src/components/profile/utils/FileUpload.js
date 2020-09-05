fdf217b0856bafe996ef3b62cde18be462f28eca
holdenmad <holden.madagame@gmail.com>
2020-09-05 11:40:08 +0200

    Fixing FileUpload.js

--- src/components/profile/utils/FileUpload.js
+++ src/components/profile/utils/FileUpload.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { AppContext } from '../../Context/AppContext';
 
 const FileUpload = () => {
-  const { state, setState } = useContext(AppContext);
+  const { state, setState, handleProfileFormChange } = useContext(AppContext);
   //fetch request from server for multer logic
   //   useEffect(() => {
   //     const requestOptions = {
@@ -18,23 +18,26 @@ const FileUpload = () => {
   //   }, []);
 
   const onFileChange = e => {
-    setState({ profileImg: e.target.files[0] });
+    setState({ ...state, profileImg: e.target.files[0] });
+    console.log(state, e.target.files[0]);
   };
 
   const onSubmit = e => {
     e.preventDefault();
+    const profileData = new FormData(); //what is this?
+    profileData.append('profileImg', state.profileImg); //can we use this?
+    console.log(state);
     //how to write "If there is a profileImg, change it, if there isn't one, create it"?
     const requestOptions = {
-      method: 'GET',
+      method: 'PUT',
       headers: { 'Content-Type': 'application/json' }
     };
-    fetch(`http://localhost:5000/profiles/${state.user._id}`, requestOptions)
+    fetch(`http://localhost:5000/profiles/${state.user._id}`, profileData, requestOptions)
       .then(res => res.json())
       .then(profile =>
         setState(previousState => ({ ...previousState, profile }))
       );
-    const profileData = new FormData(); //what is this?
-    profileData.append('profileImg', state.profile.profileImg); //can we use this?
+   
   };
 
   console.log(state.profile);

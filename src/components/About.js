ab92dc043b1c7c5b9329650d6c8e7f8ad20152b5
holdenmad <holden.madagame@gmail.com>
2020-09-08 22:23:57 +0200

    Put About on Welcome Page

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const About = () => {
+  
   return (
     <div>
       <h2>About Lola</h2>
@@ -37,6 +38,7 @@ const About = () => {
           </a>
         </div>
       </Container>
+      {/* {!state.isLoggedIn && <Link to="/"><button className="">Back to Welcome Page</button></Link>} */}
     </div>
   );
 };

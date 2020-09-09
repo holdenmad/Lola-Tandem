d19a4ea5eb68be6247c3c32d5b05a0387c73049b
holdenmad <holden.madagame@gmail.com>
2020-09-09 22:40:04 +0200

    Fix images

--- src/components/MatchedUser.js
+++ src/components/MatchedUser.js
@@ -44,7 +44,6 @@ const MatchedUser = ({ match: { user } }) => {
               className='card royalpurple-bg border border-0 shadow m-3'
               style={{ width: '35rem' }}
             >
-
               <div>
                 <div className='card-body d-flex flex-row'>
                   <div className='flex-grow-1 pl-2'>
@@ -53,13 +52,13 @@ const MatchedUser = ({ match: { user } }) => {
                       <small>{user ? calculateAge(user.birthday) : null}</small>
                     </p>
                     <p className='card-text strong-orange'>
-                      <i className='pr-2'>{`${user ? user.location : null}`},</i>
-
+                      <i className='pr-2'>
+                        {`${user ? user.location : null}`},
+                      </i>
                       <i>{`${user ? user.gender : null}`}</i>
                     </p>
                   </div>
                   <div className='justify-content-end'>
-
                     <img
                       //HARD CODED FOR DEMO
                       src={`https://i.ibb.co/K23FS19/2.png`}
@@ -69,63 +68,56 @@ const MatchedUser = ({ match: { user } }) => {
                       alt={`${user.name}`}
                     />
                   </div>
-                  <div
-                    className='bg-light p-1'
-                    aria-label='Profile information of user'
-                  >
-                    <div className='profile'>
-                      <div className='row'>
-                        <div className='col-4 speechbubble'>
-                          <i className='far fa-comments'></i>
+                </div>
+                <div
+                  className='bg-light p-1'
+                  aria-label='Profile information of user'
+                >
+                  <div className='profile'>
+                    <div className='row'>
+                      <div className='col-4 speechbubble'>
+                        <i className='far fa-comments'></i>
                       </div>
-                      <div className="col-8 speechtext">
-                        <div className="">
-                          ...native in<span className="font-weight-bold pl-1">{` ${
+                      <div className='col-8 speechtext'>
+                        <div className=''>
+                          ...native in
+                          <span className='font-weight-bold pl-1'>{` ${
                             user ? user.nativelang : null
-                            }`}</span>
-
+                          }`}</span>
                         </div>
-                        <div className='col-8 speechtext'>
-                          <div className=''>
-                            ...native in
-                            <span className='font-weight-bold pl-1'>{` ${
-                              user ? user.nativelang : null
-                            }`}</span>
-                          </div>
-                          <div>
-                            ...learning
-                            <span className='font-weight-bold pl-1'>{`${
-                              user ? user.learnlangs : null
-                            }`}</span>
-                          </div>
+                        <div>
+                          ...learning
+                          <span className='font-weight-bold pl-1'>{`${
+                            user ? user.learnlangs : null
+                          }`}</span>
                         </div>
                       </div>
-                      <hr />
+                    </div>
+                    <hr />
 
-                      <div className='row d-flex flex-row-reverse'>
-                        <div className='col-4 speechbubble2'>
-                          <i className='fas fa-icons'></i>
-                        </div>
-                        <div className='col-8 speechtext'>
-                          <div>
-                            ...interested in
-                            <span className='font-weight-bold pl-1'>{`${
-                              user ? user.interests : null
-                            }`}</span>
-                          </div>
+                    <div className='row d-flex flex-row-reverse'>
+                      <div className='col-4 speechbubble2'>
+                        <i className='fas fa-icons'></i>
+                      </div>
+                      <div className='col-8 speechtext'>
+                        <div>
+                          ...interested in
+                          <span className='font-weight-bold pl-1'>{`${
+                            user ? user.interests : null
+                          }`}</span>
                         </div>
                       </div>
-                      <hr />
+                    </div>
+                    <hr />
 
-                      <div className='row pb-4'>
-                        <div className='col-4 speechbubble'>
-                          <i class='far fa-grin-beam'></i>
-                        </div>
-                        <div className='col-8 speechtext'>
-                          <div className=''>
-                            {`${user ? user.freetext1 : null}`.substring(0, 60)}{' '}
-                            ...
-                          </div>
+                    <div className='row pb-4'>
+                      <div className='col-4 speechbubble'>
+                        <i class='far fa-grin-beam'></i>
+                      </div>
+                      <div className='col-8 speechtext'>
+                        <div className=''>
+                          {`${user ? user.freetext1 : null}`.substring(0, 60)}{' '}
+                          ...
                         </div>
                       </div>
                     </div>
